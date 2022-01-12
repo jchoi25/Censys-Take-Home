558481970350cf0824c364b602359b102554d20b
jchoi25 <juyoung7@illinois.edu>
2022-01-12 00:38:16 -0600

    Finished making script

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from censys.search import CensysCertificates
+from pandas import DataFrame
+from pathlib import Path
+
+c = CensysCertificates()
+
+fields = [
+    "parsed.fingerprint_sha256",
+    "parsed.validity.start",
+    "parsed.validity.end",
+]
+
+df = DataFrame(
+    c.search("parsed.names: censys.io and tags: trusted", fields))
+filepath = Path('./out.csv')
+
+df.to_csv(filepath, index=False, columns=fields)
